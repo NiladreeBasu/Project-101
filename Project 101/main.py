0568f1bf3f6bba179f7c20feb55667a510afe440
NiladreeBasu <69314575+NiladreeBasu@users.noreply.github.com>
2021-08-28 04:00:04 -0700

    Add files via upload

---
+++ Project 101/main.py
@@ -0,0 +1,14 @@
+from os import path
+import dropbox
+import os
+
+class TransferData:
+    name = input("Please enter file name")
+    path = input("Please enter file path")
+
+    os.path.join(path, name)
+
+    os.walk()
+
+    def uploadFiles():
+        accesstoken = ""
